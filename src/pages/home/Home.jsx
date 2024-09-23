b44507e1759979c3b9102e6e1562e76fc6e0dac9
Row-X <85471971+row-x@users.noreply.github.com>
2024-09-24 02:49:43 +0600

    Add files via upload

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,131 @@
+import BannerSlider from "../../../components/home/bannerSlider/BannerSlider";
+import SecondaryBanner from "../../../components/home/secondaryBanner/SecondaryBanner";
+import GameCard from "../../../components/shared/gameCard/GameCard";
+
+const Home = () => {
+  return (
+    <div>
+      <BannerSlider />
+      <div className="container mx-auto px-4 sm:px-10 lg:px-24">
+        <SecondaryBanner />
+        <h2 className="py-2 text-[27px] font-medium">হট গেমস</h2>
+        <div className="pb-10 grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4 lg:gap-6">
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_lightning_storm/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Lightning Storm"}
+            gameText={"EVOLUTION GAMING"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
