97758da85459c608e72776ed3ee4dc581f066e13
Row-X <85471971+row-x@users.noreply.github.com>
2024-09-24 02:47:09 +0600

    Add files via upload

---
+++ src/pages/casino/Casino.jsx
@@ -0,0 +1,217 @@
+import GameVendorList from "@/components/home/gameVendorList/GameVendorList";
+import Banner from "../../../components/shared/banner/Banner";
+import GameCard from "../../../components/shared/gameCard/GameCard";
+
+const Casino = () => {
+  const buttons = [
+    {
+      text: "সব",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/evo.svg",
+      text: "Evolution Gaming",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/pp.svg",
+      text: "Pragmatic Play",
+    },
+    {
+      image:
+        "https://jiliwin.9terawolf.com/images/babu/provider/ld/sexy_v2.svg",
+      text: "AE Casino",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/royal.svg",
+      text: "Royal",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/ezugi.svg",
+      text: "Ezugi",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/pt.svg",
+      text: "PlayTech",
+    },
+    {
+      image: "https://jiliwin.9terawolf.com/images/babu/provider/ld/aura.svg",
+      text: "Aura",
+    },
+  ];
+  return (
+    <div>
+      <Banner
+        B_image={
+          "https://jiliwin.9terawolf.com/images/babu/game_banner/ld_new.jpg"
+        }
+        B_heading={"ক্যাসিনো"}
+        B_semiText={
+          "BABU88 এর সাথে লাইভ ডিলার এবং অন্যান্য আসল খেলোয়াড়দের সাথে খেলুন"
+        }
+        B_text={
+          "ক্রেজি টাইম, অন্দর বাহার এবং লাইটনিং রুলেটের মতো সব বড় হিটগুলি উপভোগ করুন!"
+        }
+      />
+      <GameVendorList buttons={buttons} />
+      <div className="container mx-auto px-4 sm:px-10 lg:px-24">
+        <div className="mt-10 pb-10 grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4 lg:gap-6">
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={
+              "https://www.babu88.app/static/svg/game-icon-new-mobile.svg"
+            }
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={
+              "https://www.babu88.app/static/svg/game-icon-new-mobile.svg"
+            }
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={
+              "https://www.babu88.app/static/svg/game-icon-new-mobile.svg"
+            }
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/ppl_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={
+              "https://www.babu88.app/static/svg/game-icon-new-mobile.svg"
+            }
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={"https://www.babu88.app/static/image/other/hot-icon.png"}
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+          <GameCard
+            gameCardImg={
+              "https://luckmedia.link/evo_top_games_lobby/thumb.webp"
+            }
+            gameHot={
+              "https://www.babu88.app/static/svg/game-icon-new-mobile.svg"
+            }
+            gameHeading={"Live Lobby"}
+            headingCenter={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Casino;
